cdc3b8141557682ce56486ae6563a940153180d4
Swarupwasnik <99977117+Swarupwasnik@users.noreply.github.com>
2022-07-21 02:17:36 +0530

    make it better

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express =require('express');
 const app=express();
 const  mixrouter=require("./route/mix")
 let cors=require("cors");
+let port = process.env.PORT || 3005;
 app.use(cors())
 app.use("/api",mixrouter)
 
-app.listen(3005,()=>{
+app.listen(port,()=>{
     console.log('i working on 3005');
 })
